39382d2403497f0b291e6b496d1c4edcf8017349
M Krishna Chaitanya <mkc@KCs-MacBook-Air.local>
2022-03-30 22:23:45 +0530

    HIIIII

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
 let city = document.querySelector('.city');
-let date = document.querySelector('.date');
-let div = document.querySelector('.result');
-let divHead = document.querySelector('.resHead');
 
-let getWeather = () => {
-    divHead.innerHTML = '';
-    div.innerHTML = '';
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=41202df047fd63403bdfb9681c6c9bdb&units=metric`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            let list = data.list;
-            let count = 0;
-            divHead.innerHTML = `${data.city.name.toUpperCase()}`;
-            divHead.innerHTML += `<p>Date : ${rev(date.value)[0]}</p>`
-            for (let i = 0; i < data.list.length; i++) {
-                if (list[i].dt_txt.includes(date.value)) {
-                    let dt = rev(list[i].dt_txt);
-                    let image = document.createElement('img');
-                    let sect = document.createElement('div');
-                    image.setAttribute('src', `https://openweathermap.org/img/wn/${list[i].weather[0].icon}@2x.png`);
-                    sect.setAttribute('class', 'details');
-                    sect.innerHTML = `${dt[1]}<br>
-                    TEMP : ${list[i].main.temp} ˚ C<br>`;
-                    sect.append(image);
-                    sect.innerHTML += `<br>${list[i].weather[0].description}`;
-                    count++;
-                    div.append(sect);
-                }
-            }
-            if (count === 0) {
-                div.innerHTML = 'Data available only for 5 days from today';
-            }
-        })
+let graphValues = async (data) => {
+    let xvalues = [], yvalues = [];
+    for (let i = 0; i < data.length; i += 8) {
+        xvalues.push(data[i].dt_txt.substring(0, 10));
+        yvalues.push(data[i].main.temp);
+    }
+    return { xvalues, yvalues }
 }
 
-let rev = (str) => {
-    let dt = str.substring(0, 10);
-    let time = str.substring(11);
-    let dtarr = [dt.substring(0, 4), '/', dt.substring(5, 7), '/', dt.substring(8)]
-    dtarr = dtarr.reverse();
-    dt = dtarr.join('');
-    return [dt, time];
+let getWeather = async () => {
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=41202df047fd63403bdfb9681c6c9bdb&units=metric`);
+    let resdata = await res.json();
+    let list = resdata.list;
+    let graphData = await graphValues(list);
+    console.log(graphData)
+    const graph = {
+        labels: graphData.xvalues,
+        dataSets: [{
+            label: 'Temp',
+            data: graphData.yvalues,
+            backgroundColor: [
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(201, 203, 207, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 95, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(201, 203, 207)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 95, 132)'
+            ],
+            borderWidth: 1
+        }]
+    }
+    const config = {
+        type: 'bar',
+        data: graph,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    };
+    const myChart = new Chart(document.getElementById('barGraph').getContext('2d'), config);
 }
